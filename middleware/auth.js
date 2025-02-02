1d603e4addc8e066e1d3aa4c15f52bd9bea9afb6
krishalsatta <sattakrishal97@gmail.com>
2025-02-02 10:24:31 +0545

    commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const authGuard = (req, res, next) => {
-  // check if auth header is present
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     return res.status(401).json({
@@ -10,9 +9,7 @@ const authGuard = (req, res, next) => {
     });
   }
 
-  // split the auth header and get the token
   const token = authHeader.split(" ")[1];
-  // check if token is present
   if (!token) {
     return res.status(401).json({
       success: false,
